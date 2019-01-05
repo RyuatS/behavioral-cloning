51ca3715aad1afa4676259c5be7a7019fb90c441
ryutaShitomi <sitomi0216@icloud.com>
2019-01-06 08:28:24 +0900

    add comment

--- image_generator.py
+++ image_generator.py
@@ -4,12 +4,19 @@ import numpy as np
 import random
 import os
 
+
 def generator(samples, batch_size=32, data_augmentation=False, is_exist_log=False):
+    """
+    this is the function for creating generators.
+    if data_augmentation is True, flip the image through to increase the image.
+
+    return => (image_array, angles_array)
+    """
     num_samples = len(samples)
-    
+
     while 1:
         random.shuffle(samples)
-        
+
         if data_augmentation:
             for offset in range(0, num_samples, int(batch_size//6)):
                 batch_samples = samples[offset:offset+batch_size]
@@ -22,7 +29,7 @@ def generator(samples, batch_size=32, data_augmentation=False, is_exist_log=Fals
                     right_basename = batch_sample[2].split('/')[-1]
                     if is_exist_log:
                         center_filename = '../../IMG/' + center_basename
-                        left_filename = '../../IMG/' + left_basename 
+                        left_filename = '../../IMG/' + left_basename
                         right_filename = '../../IMG/' + right_basename
                     else:
                         center_filename = './data/IMG/' + center_basename
@@ -37,10 +44,10 @@ def generator(samples, batch_size=32, data_augmentation=False, is_exist_log=Fals
                     left_image = cv2.cvtColor(left_image, cv2.COLOR_BGR2RGB)
                     right_image = cv2.cvtColor(right_image, cv2.COLOR_BGR2RGB)
                     # flip image
-                    center_image_flipped = cv2.flip(center_image, 1) # flip image
+                    center_image_flipped = cv2.flip(center_image, 1)
                     left_image_flipped = cv2.flip(left_image, 1)
                     right_image_flipped = cv2.flip(right_image, 1)
-                    
+
                     # calc angle
                     center_angle = float(batch_sample[3])
                     center_angle_flipped = -center_angle
@@ -49,15 +56,15 @@ def generator(samples, batch_size=32, data_augmentation=False, is_exist_log=Fals
                     left_angle_flipped = -left_angle
                     right_angle = center_angle - correction
                     right_angle_flipped = -right_angle
-                    
-                    
+
+
                     images.append(center_image)
                     images.append(center_image_flipped)
                     images.append(left_image)
                     images.append(left_image_flipped)
                     images.append(right_image)
                     images.append(right_image_flipped)
-                    
+
                     angles.append(center_angle)
                     angles.append(center_angle_flipped)
                     angles.append(left_angle)
@@ -65,14 +72,14 @@ def generator(samples, batch_size=32, data_augmentation=False, is_exist_log=Fals
                     angles.append(right_angle)
                     angles.append(right_angle_flipped)
 
-                # trim image to only see section with road 
+                # trim image to only see section with road
                 X_train = np.array(images)
                 y_train = np.array(angles)
                 yield sklearn.utils.shuffle(X_train, y_train)
-        
+
         else:
             for offset in range(0, num_samples, batch_size):
-                batch_samples = samples[offset:offset+batch_size]                
+                batch_samples = samples[offset:offset+batch_size]
                 images = []
                 angles = []
                 for batch_sample in batch_samples:
@@ -88,9 +95,7 @@ def generator(samples, batch_size=32, data_augmentation=False, is_exist_log=Fals
                     angles.append(center_angle)
 
 
-                # trim image to only see section with road 
+                # trim image to only see section with road
                 X_train = np.array(images)
                 y_train = np.array(angles)
                 yield sklearn.utils.shuffle(X_train, y_train)
-
-    
--- train.py
+++ train.py
@@ -2,14 +2,17 @@ from image_generator import generator
 import csv
 import cv2
 import os
-from sklearn.model_selection import train_test_split 
+from sklearn.model_selection import train_test_split
 from models import TransferVGG, Nvidia
 import sys
 
-
+# This array has the each row in csv file.
 samples = []
 
+# Does the own collection data exist?
+# exist -> True, not exist -> False.
 is_exist_log = 'driving_log.csv' in os.listdir('//home')
+
 
 if is_exist_log:
     print('load the my data')
@@ -25,42 +28,43 @@ else:
         header = next(reader)
         for line in reader:
             samples.append(line)
-#     print('Error: driving_log.csv is not existed.')
-#     sys.exit(0)
 
+# print amount of the data.
 print('amount of sample -> {}'.format(len(samples)))
 
 
-# Trimmed image format 
-height, width, ch = 160, 320, 3
-input_size = 100
-
+# if you want to use the transfer-learning model. comment in below.
 # model = TransferVGG(128, 32)
+
+# use Nvidia model from model.py.
 model = Nvidia()
 
-save_path = './train_models/model3.h5'
+# file_path for saving model's weights.
+save_path = './train_models/model.h5'
 
-# Train the model
-batch_size =256
-epochs = 10
-
-
+# if file_path is exist in train_models directory, load the file.
 if os.path.basename(save_path) in os.listdir('./train_models'):
     model.load(save_path)
-    
 
+# split the data set to training and validation data.
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
+# hyper parameter
+batch_size = 32
+epochs = 10
+
+# create generator
 train_generator = generator(train_samples, batch_size=batch_size, data_augmentation=True, is_exist_log=is_exist_log)
 validation_generator = generator(validation_samples, batch_size=batch_size, is_exist_log=is_exist_log)
+
+
 try:
+    # train the model
     model.train(train_generator, validation_generator, epochs, len(train_samples)*6, len(validation_samples), batch_size)
 except KeyboardInterrupt:
     model.save(save_path)
     sys.exit(0)
 
+# save the model
 model.save(save_path)
 model.plot_error()
-
-
-
